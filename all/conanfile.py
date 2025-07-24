458b07a618c468e37b2d4806fad81444af25a5dc
werto87 <huhulasmichinruhe@web.de>
2025-07-24 23:22:47 +0200

    init

--- all/conanfile.py
+++ all/conanfile.py
@@ -1,30 +1,57 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, cmake_layout
 from conan.tools.files import get
-
+from conan.tools.cmake import CMakeToolchain
+import os
+import subprocess
 required_conan_version = ">=1.51.1"
 
 
-class ConfuSociConan(ConanFile):
-    name = "lib_name"
+class ModernDurakUnrealCxx(ConanFile):
+    name = "modern_durak_game"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": False, "fPIC": True}
-    generators = "CMakeDeps", "CMakeToolchain"
+    generators = "CMakeDeps"
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.user_presets_path = False #workaround because this leads to useless options in cmake-tools configure
+        tc.generate()
 
     def config_options(self):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
     def configure(self):
-        self.options["soci"].with_boost = True
-        self.options["soci"].with_sqlite3 = True
+        self.options["catch2"].with_main = True
+        self.options["catch2"].with_benchmark = True
 
     def requirements(self):
-        self.requires("boost/1.83.0")
+        self.requires("durak/1.2.0",force=True,transitive_headers=True)
+        self.requires("magic_enum/0.9.6")
+        self.requires("boost/1.86.0",force=True,transitive_headers=True)
+        self.requires("confu_json/[>=1.1.1 <2]@modern-durak",force=True)
+        self.requires("sml/1.1.8") #DO NOT CHANGE THIS. starting with version 1.1.9 process_event returns ins some cases false where before it returned true
+        self.requires("durak_computer_controlled_opponent/2.2.2")
+        self.requires("confu_soci/[<1]")
+        self.requires("corrade/2025.06")
+        self.requires("modern_durak_game_shared/latest",transitive_headers=True)
+        self.requires("my_web_socket/0.1.3",transitive_headers=True)
 
     def source(self):
-        get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        token = os.getenv("GIT_TOKEN")
+        if not token:
+            raise Exception("Missing GIT_TOKEN environment variable")
+
+        user = "werto87"
+        repo_name = "modern_durak_game"
+        tag = f"v{self.version}"  # Add 'v' prefix to version
+        url = f"https://{token}@github.com/{user}/{repo_name}.git"
+        self.output.info(f"Cloning tag '{tag}' from {user}/{repo_name}")
+        subprocess.run([
+            "git", "clone", "--branch", tag, "--depth", "1", url, "."
+        ], check=True)
 
     def build(self):
         cmake = CMake(self)
@@ -40,3 +67,4 @@ class ConfuSociConan(ConanFile):
 
     def package_info(self):
         self.cpp_info.libs = [self.name]
+        
--- all/test_package/test_package.cpp
+++ all/test_package/test_package.cpp
@@ -1,13 +1,9 @@
-#include <confu_soci/convenienceFunctionForSoci.hxx>
+#include <modern_durak_game/server/server.hxx>
 
 BOOST_FUSION_DEFINE_STRUCT ((test), Player, (std::string, playerId) )
 
 int main() {
-  std::cout<<confu_soci::typeNameWithOutNamespace (test::Player{})<<std::endl;
-  if (confu_soci::typeNameWithOutNamespace (test::Player{})!= "Player")
-  {
-    std::terminate();
-  }
+  [[maybe_unused]]auto server=modern_durak_game::Server{};
 
   return 0;
 }
